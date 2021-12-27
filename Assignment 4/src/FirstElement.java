f568f653abe6b0cf7792396343d32025c944c05e
Avinash_Kale <avinash.kale@prorigosoftware.com>
2021-12-28 01:12:44 +0530

    Assingement 4 is done

---
+++ Assignment 4/src/FirstElement.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+
+
+public class FirstElement {
+
+	public static void main(String[] args) {
+		
+		
+		List<Integer> numbers = Arrays.asList(88, 2, 1, 3, 3, 2, 4,5,7,8,41,44,55,67,64,222,111,14444,15555,31111,1,21,14,100);
+		System.out.println(" First element of the list:");
+         numbers.stream()
+               .findFirst()
+               .ifPresent(System.out::println);
+
+	}
+
+}
